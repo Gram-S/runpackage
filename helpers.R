aad59fd3d846e0de16f01f8bdd1878aa149817ab
Gram-S <grantms4455@gmail.com>
2025-08-31 21:38:23 -0600

    Add function checking to make sure everything is profiled

--- helpers.R
+++ helpers.R
@@ -25,3 +25,10 @@ docu.end <- function(path.to.file, func.name){
   write.me <- c(txt.from.file, newline) # Add the new line to the text
   writeLines(write.me, path.to.file)
 }
+
+query.continue <- function(...){
+  cat(...)
+  answer = readline()
+  continue <- answer %in% c("Yes", "yes", "y")
+  if(!continue) stop("User did not affirm. Code Terminated")
+}
--- runpackage.R
+++ runpackage.R
@@ -4,18 +4,23 @@
       #* ptmtable.rda
       #* [add more]
 
+# The sources folder is optional, but having it will allow you to view the source code of the functions and how much time they take rather then their subfuctions, so it's highly recommended
+
 # The log folder (formatted mm-dd-yyyy at [time]) will have: 
       #* Env_Image.rda | A snapshot of the global enviroment at the time this script ends which includes every object created in the functions
       #* Pr_Output.txt | The "natural" output of every print statement in the functions. (This will NOT include things output with "message" function)
       #* Progress.txt | A file that updates in real time as the code progresses. The last line is what the script is currently doing. Please check this to check the progress
-      #* PTMsTP_Profile | The output of the R profiler for all operations done. Can be viewed with the command: [profvis::profvis(prof_input = "logs/PTMsTP_profile")]
-
+      #* profile.html | The output of the R profiler for all operations done. Please view this
 
 # For organizational purposes, some commonly used functions are in the helpers file
 source("helpers.R")
+system(" Rscript -e 'options(keep.source=TRUE)' ")
+functions.to.run <- c() #TO DO - Check to make sure every function that needs to be ran is sourced!
+query.continue("These are all functions that will be ran and profiled. Please make sure there are none missing: ", sapply(ls(), paste, "\n"), "\n Continue? (Yes/No)")
+
 
 # Error catch for dependencies so that the code does not stop after 5 days because a package wasn't loaded
-dependencies <-  c("PTMsToPathways", "igraph", "plyr", "purrr", "Rtsne", "vegan", "dplyr", "utils", "BiocManager", "STRINGdb", "RCy3")
+dependencies <-  c("PTMsToPathways", "profvis", "igraph", "plyr", "purrr", "Rtsne", "vegan", "dplyr", "utils", "BiocManager", "STRINGdb", "RCy3")
 required.dependencies <- setdiff(dependencies, rownames(installed.packages()))
 if(length(required.dependencies) >= 1) stop(cat("The following packages have not been found: ", required.dependencies, "Note: STRINGdb and RCy3 must be installed with BiocManager.")) 
 lapply(dependencies, library, character.only=TRUE)
@@ -38,13 +43,12 @@ ptmtable.name <- load("data/ptmtable.rda", verbose=TRUE)
 
 
 # Running the profiler over the code
-profvis::profvis({
+myprofile <- profvis(
   {
-    docu.start(path, "MakeClusterList")
     MakeClusterList(get(ptmtable.name))
-    docu.end(path, "MakeClusterList")
+    
   }
-}, prof_output = path.to.new.file(logs.directory, "PTMsTP_profile"))
+)
 
 
 
@@ -57,7 +61,8 @@ profvis::profvis({
 
 # Save Global Enviroment to a file
 save.image(file=path.to.new.file(logs.directory, "Env_Image.rda"))
+htmlwidgets::saveWidget(myprofile, path.to.new.file(logs.directory, "profile.html"))
 
 # Cleanup
 sink() # DO NOT FORGET
-rm("docu.start", "docu.end", "get.time", "path.to.new.file")
+cat("Log file:" , logs.directory)
